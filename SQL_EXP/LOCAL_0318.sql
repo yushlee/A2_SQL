527a7661be1f4d6e606ace15c83404421fb13e24
yushlee <ktbwn@hotmail.com>
2023-03-18 17:18:00 +0800

    LOCAL_0318.sql

--- SQL_EXP/LOCAL_0318.sql
+++ SQL_EXP/LOCAL_0318.sql
@@ -219,7 +219,156 @@ SELECT STORE_ID, STORE_NAME, SALES,
     ROW_NUMBER()OVER(ORDER BY SALES DESC) "STORE_ROW_NUMBER"
 FROM STORE_INFORMATION;
 
--- Aggregate Functions with OVER clause
+-- Aggregate Functions with OVER clause (聚合函數)
+SELECT STORE_ID, STORE_NAME, SALES, GEOGRAPHY_ID,
+    -- 依「區域劃分」取營業額"最小值"
+    MIN(SALES) OVER (PARTITION BY GEOGRAPHY_ID) MIN_SALES,
+    -- 依「區域劃分」取營業額"最大值"
+    MAX(SALES) OVER (PARTITION BY GEOGRAPHY_ID) MAX_SALES,
+    -- 依「區域劃分」取商店"數量"
+    COUNT(STORE_ID) OVER (PARTITION BY GEOGRAPHY_ID) COUNT_STORE_ID,
+    -- 依「區域劃分」取營業額"總和"
+    SUM(SALES) OVER (PARTITION BY GEOGRAPHY_ID) SUM_SALES,
+    -- 依「區域劃分」取營業額"平均"
+    AVG(SALES) OVER (PARTITION BY GEOGRAPHY_ID) AVG_SALES,
+	ROUND(AVG(SALES) OVER (PARTITION BY GEOGRAPHY_ID), 1) ROUND_AVG_SALES
+FROM STORE_INFORMATION
+ORDER BY GEOGRAPHY_ID, SALES;
 
 
--- Analytic Functions with OVER clause
+SELECT * FROM STORE_INFORMATION;
+
+-- Analytic Functions with OVER clause(分析函數)
+SELECT STORE_ID, STORE_NAME,
+    ROW_NUMBER( ) OVER (ORDER BY SALES) ROWNO_STORE,
+    SALES,
+    -- 依「營業額」排序取"上一個"營業額
+    LAG(SALES) OVER (ORDER BY SALES) PREV_SALES,
+    -- 依「營業額」排序取"下一個"營業額
+    LEAD(SALES) OVER (ORDER BY SALES) NEXT_SALES
+FROM STORE_INFORMATION
+ORDER BY SALES;
+
+
+-- CEIL(x)：返回大於或等於x的最大整數值(無條件進位)
+-- FLOOR(x)：返回小於或等於x的最小整數值(無條件捨去)
+-- ROUND(x ,y)：
+-- 返回(四捨五入)到小數點右邊y位的x值,y預設值為0
+-- 如果y是負數，則捨入到小數點左邊相應的整數位上
+
+SELECT CEIL(123.4), FLOOR(123.9), 
+	ROUND(123.5), ROUND(1234.56, 1), ROUND(1234.54, 1),
+    ROUND(1234, -2), ROUND(1254, -2);
+    
+    
+-- MySQL轉換函數：
+-- 1.DATE_FORMAT(date,format):日期轉字串
+SELECT SYSDATE();
+SELECT DATE_FORMAT(SYSDATE(), '%m-%d-%Y %T'),
+-- 2.STR_TO_DATE(str,format):字串轉日期
+STR_TO_DATE('2021-08-15 00:00:00', "%Y-%m-%d %T"),
+DATE_ADD(STR_TO_DATE('2021-08-15 00:00:00', "%Y-%m-%d %T"), INTERVAL 5 DAY);
+
+-- Oracle 轉換函數：
+-- 1.TO_CHAR(d , format)：日期轉字串
+/*
+SELECT TO_CHAR(SYSDATE,'YYYY-mm-DD HH24:MI:SS'),
+  -- 2.TO_DATE(string ,format)：字串轉日期
+  TO_DATE('2018-05-15 12:30:23','YYYY-mm-DD HH24:MI:SS'),
+  -- 3.TO_TIMESTAMP(string,  [format])：字串轉日期
+  TO_TIMESTAMP( '2011-12-23 12:30:23.852', 'YYYY-MM-DD HH24:MI:SS.FF3')
+FROM DUAL;
+*/
+
+-- MS SQL
+-- 1.日期轉字串
+/*
+SELECT convert(varchar, getdate(), 100) 'mon dd yyyy hh:mmAM (or PM)';
+SELECT convert(varchar, getdate(), 101) 'mm/dd/yyyy';
+SELECT convert(varchar, getdate(), 102) 'yyyy.mm.dd';
+SELECT convert(varchar, getdate(), 103) 'dd/mm/yyyy';
+SELECT convert(varchar, getdate(), 104) 'dd.mm.yyyy';
+SELECT convert(varchar, getdate(), 105) 'dd-mm-yyyy';
+SELECT convert(varchar, getdate(), 106) 'dd mon yyyy';
+SELECT convert(varchar, getdate(), 107) 'mon dd, yyyy';
+SELECT convert(varchar, getdate(), 108) 'hh:mm:ss';
+SELECT convert(varchar, getdate(), 109) 'mon dd yyyy hh:mm:ss:mmmAM (or PM)';
+SELECT convert(varchar, getdate(), 110) 'mm-dd-yyyy';
+SELECT convert(varchar, getdate(), 111) 'yyyy/mm/dd';
+SELECT convert(varchar, getdate(), 112) 'yyyymmdd';
+SELECT convert(varchar, getdate(), 113) 'dd mon yyyy hh:mm:ss:mmm';
+SELECT convert(varchar, getdate(), 114) 'hh:mm:ss:mmm(24h)';
+SELECT convert(varchar, getdate(), 120) 'yyyy-mm-dd hh:mm:ss(24h)';
+SELECT convert(varchar, getdate(), 121) 'yyyy-mm-dd hh:mm:ss.mmm';
+SELECT convert(varchar, getdate(), 126) 'yyyy-mm-ddThh:mm:ss.mmm';
+
+-- 2.字串轉日期
+SELECT convert(datetime, '2021-08-16 21:52:22', 120)　'yyyy-mm-dd hh:mm:ss(24h)';
+*/
+
+-- MySQL
+SELECT SYSDATE(), YEAR(SYSDATE()), MONTH(SYSDATE()), DAY(SYSDATE()),
+HOUR(SYSDATE()), MINUTE(SYSDATE()), SECOND(SYSDATE());
+
+SELECT STORE_ID, STORE_NAME, STORE_DATE
+FROM STORE_INFORMATION
+WHERE YEAR(STORE_DATE) = 2018 
+AND MONTH(STORE_DATE) BETWEEN 1 AND 3;
+
+
+-- oracle
+-- TRUNC(date, [format])：對日期作無條件捨去運算
+-- MONTH(月捨去)、YEAR(年捨去)、不帶FORMAT(日捨去)
+/*
+SELECT SYSDATE, TRUNC(SYSDATE), TRUNC(SYSDATE, 'MONTH'), TRUNC(SYSDATE, 'YEAR')
+FROM DUAL;
+*/
+
+-- MS SQL
+/*
+SELECT GETDATE(), YEAR(GETDATE()), MONTH(GETDATE()), DAY(GETDATE());
+
+SELECT GETDATE() 'Today',
+DATEPART(year,GETDATE()) 'Year Part',
+DATEPART(month,GETDATE()) 'Month Part',
+DATEPART(day,GETDATE()) 'Day Part',
+DATEPART(hour,GETDATE()) 'Hour Part',
+DATEPART(minute,GETDATE()) 'Minute Part',
+DATEPART(second,GETDATE()) 'Second Part',
+DATEPART(millisecond,GETDATE()) 'MilliSecond Part';
+*/
+
+--  MySQL日期算術:
+SELECT SYSDATE(), 
+DATE_ADD(SYSDATE(), INTERVAL 1 DAY),
+DATE_ADD(SYSDATE(), INTERVAL 1 MINUTE),
+DATE_ADD(SYSDATE(), INTERVAL -1 DAY),
+DATE_ADD(SYSDATE(), INTERVAL -1 MINUTE);
+
+-- Oracle INTERVAL 日期計算
+/*
+SELECT SYSDATE,
+SYSDATE - INTERVAL '1' YEAR,
+SYSDATE + INTERVAL '1' YEAR,
+SYSDATE - INTERVAL '1' MONTH,
+SYSDATE - INTERVAL '1' DAY,
+SYSDATE - INTERVAL '1' HOUR,
+SYSDATE - INTERVAL '1' MINUTE,
+SYSDATE - INTERVAL '1' SECOND
+FROM DUAL;
+*/
+
+-- MS SQL
+/*
+SELECT GETDATE(),
+DATEADD(YEAR, 1, GETDATE()) "DATEADD_YEAR",
+DATEADD(MONTH, 1, GETDATE()) "DATEADD_MONTH",
+DATEADD(DAY, 1, GETDATE()) "DATEADD_DAY",
+DATEADD(HOUR, 1, GETDATE()) "DATEADD_HOUR",
+DATEADD(MINUTE, 1, GETDATE()) "DATEADD_MINUTE",
+DATEADD(SECOND, 1, GETDATE()) "DATEADD_SECOND";
+
+SELECT GETDATE(),
+DATEADD(YEAR, -1, GETDATE()) "DATESUB_YEAR";
+*/
+
