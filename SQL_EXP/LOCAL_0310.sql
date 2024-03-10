1ffb7a1a8ab1043f3aae7cf088a72bcc6d4b9082
yushlee <ktbwn@hotmail.com>
2024-03-10 14:21:39 +0800

    LOCAL_0310.sql

--- SQL_EXP/LOCAL_0310.sql
+++ SQL_EXP/LOCAL_0310.sql
@@ -2,9 +2,25 @@ SELECT S.*, G.*
 FROM store_information S, geography G 
 WHERE S.GEOGRAPHY_ID = G.GEOGRAPHY_ID;
 
+-- 內部連接
+-- INNER JOIN...ON
 SELECT S.*, G.* 
-FROM store_information S JOIN geography G 
+FROM store_information S INNER JOIN geography G 
 ON S.GEOGRAPHY_ID = G.GEOGRAPHY_ID;
+
+
+SELECT G.*, S.*  
+FROM geography G  LEFT OUTER JOIN store_information S 
+ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID;
+
+SELECT G.*, S.*  
+FROM geography G  RIGHT OUTER JOIN store_information S 
+ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID;
+
+-- MySQL 不支援FULL OUTER JOIN
+-- SELECT G.*, S.*  
+-- FROM geography G  FULL OUTER JOIN store_information S 
+-- ON G.GEOGRAPHY_ID = S.GEOGRAPHY_ID;
 
 -- cross join
 SELECT S.*, G.* 
